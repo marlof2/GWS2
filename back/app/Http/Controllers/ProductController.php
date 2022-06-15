547020c3e6b84243dde9c7d83c41db0d81475899
Juliana Matos <jumatosk@gmail.com>
2022-06-15 13:35:34 -0300

    crud products

--- back/app/Http/Controllers/ProductController.php
+++ back/app/Http/Controllers/ProductController.php
@@ -15,17 +15,17 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
-    }
+        try {
+            $product = Product::all();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+            return response()->json([
+                'products' => $product
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 406);
+        }
     }
 
     /**
@@ -34,9 +34,46 @@ class ProductController extends Controller
      * @param  \App\Http\Requests\StoreProductRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreProductRequest $request)
+    public function store(StoreProductRequest $request,  Product $product)
     {
-        //
+        try {
+            $product::create($request->all());
+
+            return response()->json([
+                'products' => $product,
+                'status' => '200'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'msg' => $e->getMessage(),
+                'error_linha' => $e->getLine(),
+            ], 406);
+        }
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateProductRequest  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProductRequest $request, Product $product, $id)
+    {
+        try {
+            $product->find($id)->update($request->all());
+
+            return response()->json([
+                'message'=> 'Produto atualizado com sucesso!',
+                'status' => '200'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'msg' => $e->getMessage(),
+                'error_linha' => $e->getLine(),
+            ], 406);
+        }
     }
 
     /**
@@ -50,28 +87,6 @@ class ProductController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Product $product)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateProductRequest  $request
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateProductRequest $request, Product $product)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
@@ -79,8 +94,20 @@ class ProductController extends Controller
      * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Product $product)
+    public function destroy($id, Product $product)
     {
-        //
+        try {
+            $products = $product->find($id);
+            $products->delete();
+
+            return response()->json([
+                'message' => $products->nome.' excluído(a) com sucesso!',
+                'status' => '200'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 406);
+        }
     }
 }
